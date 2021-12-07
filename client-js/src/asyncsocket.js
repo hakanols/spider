00e5c6c050f26da7b6cec3b0544512edcaecc60a
Håkan Olsson <hakanols@gmail.com>
2021-12-07 21:48:01 +0100

    .

--- client-js/src/asyncsocket.js
+++ client-js/src/asyncsocket.js
@@ -28,7 +28,7 @@ async function lazyWebSocketLoader() {
 export async function setupWebsocket(uri){
 	await lazyWebSocketLoader()
 	let webSocket = new WebSocket(uri);
-	// webSocket.binaryType = "blob";
+	webSocket.binaryType = "blob";
 	return await new Promise(function (resolve, reject) {
 		webSocket.onclose = function(){
 			reject("Closed during connection");
