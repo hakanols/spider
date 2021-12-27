a1bc9c208d6ea5b8b967b7c472cd5e7cb6fce571
Håkan Olsson <hakan.olsson@assaabloy.com>
2021-12-27 22:26:41 +0100

    .

--- client-js/test/test.js
+++ client-js/test/test.js
@@ -8,7 +8,7 @@ const onlieServer = "wss://spider-8t2d6.ondigitalocean.app/net";
 const localServer = "ws://localhost:8080/net";
 const testServerUri = ( isLocalhost ? localServer : onlieServer );
 
-console.log('Version: 0.0.7')
+console.log('Version: 0.0.8')
 
 test('Is a spider running', async function (t) {
 	const whatToDo = 'Run from terminal: go run .';
@@ -112,7 +112,8 @@ test('Test spider', async function (t) {
 	console.log("Session up. Start 1,5s wait")
 	await util.sleep(1500)
 
-	let m1 = await hostConn.receive(50);
+	let m1 = await hostConn.receive(200);
+	t.ok(m1 != null, "m1 is not null");
 	let sessionId = m1[0];
 	console.log("Session id: " + util.ab2hex([sessionId]));
     t.equal(m1[1], messageTypeNew, "Got new session");
@@ -121,7 +122,8 @@ test('Test spider', async function (t) {
 	await util.sleep(1000)
 	let testMessage1 = util.hex2ab("deadbeef")
 	clientConn.send(testMessage1)
-	let m2 = await hostConn.receive(50);
+	let m2 = await hostConn.receive(200);
+	t.ok(m2 != null, "m2 is not null");
 	t.equal(m2[0], sessionId, "Matching session id");
 	t.equal(m2[1], messageTypeMessage, "Got new message");
 	t.arrayEqual(m2.slice(2), testMessage1, "M2 matching message");
@@ -131,11 +133,13 @@ test('Test spider', async function (t) {
 	let testMessage2 = util.hex2ab("feedcafe")
 	var message = new Uint8Array( [sessionId, messageTypeMessage, ...testMessage2]);
     hostConn.send(message)
-	let m3 = await clientConn.receive(50);
+	let m3 = await clientConn.receive(200);
+	t.ok(m3 != null, "m3 is not null");
 	t.arrayEqual(m3, testMessage2, "M3 matching message");
 
 	await clientConn.close();
-	let m4 = await hostConn.receive(50);
+	let m4 = await hostConn.receive(200);
+	t.ok(m4 != null, "m4 is not null");
 	t.arrayEqual(m4[0], sessionId, "Matching session id");
 	t.arrayEqual(m4[1], messageTypeClose, "Got session close");
 
