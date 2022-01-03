8bbd9a68859c1e6793251d78b894d058aca0aeab
Håkan Olsson <hakanols@gmail.com>
2022-01-03 17:10:20 +0100

    Testing removing lazy loader

--- client-js/src/asyncsocket.js
+++ client-js/src/asyncsocket.js
@@ -1,32 +1,24 @@
 import * as util from './../lib/util.js';
-let WebSocket = null;
+let WebSocket = await webSocketLoader();
 
 const CONNECTING = 0;
 const OPEN = 1;
 const CLOSING = 2;
 const CLOSED = 3;
 
-async function lazyWebSocketLoader() {
-	// This lazy loader exist cause a problem on iOS browsers:
-	// Safari, Firefox and Chrome. Apparently await can not be
-	// used on initialization  if the module is loaded with
-	// dynamic import (Perhaps more generic). Even if the await
-	// is hidden behind a if statement that did not help.
-	if (WebSocket == null) {
-		if (typeof window === 'undefined'){
-			console.log("Loading NodeJS Websocket module")
-			let NodeWebSocket = await import('websocket')
-			WebSocket = NodeWebSocket.default.w3cwebsocket;
-		}
-		else {
-			console.log("Loading native Browser Websocket support")
-			WebSocket = self.WebSocket;
-		}
+async function webSocketLoader() {
+	if (typeof window === 'undefined'){
+		console.log("Loading NodeJS Websocket module")
+		let NodeWebSocket = await import('websocket')
+		WebSocket = NodeWebSocket.default.w3cwebsocket;
+	}
+	else {
+		console.log("Loading native Browser Websocket support")
+		WebSocket = self.WebSocket;
 	}
 }
 
 export async function setupWebsocket(uri){
-	await lazyWebSocketLoader()
 	let webSocket = new WebSocket(uri);
 	webSocket.binaryType = "arraybuffer";
 	return await new Promise(function (resolve, reject) {
